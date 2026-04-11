6c724fc1a3e1ff66e959f66af103801aa5d97d86
[Aarush Lohani] <[aarush.lohani2@gmail.com]>
2026-04-11 10:50:02 +0530

    x

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use('/api/auth', require('./routes/AuthRoutes'))
+app.use('/api/auth', require('./routes/authRoutes'))
 app.use('/api/companies', require('./routes/companyRoutes'))  
 app.use('/api/drives', require('./routes/driveRoutes'))
 app.use('/api/applications', require('./routes/applicationRoutes'))
